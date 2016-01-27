6a9667730c2c328ccbae98ab630b1a7d2fa42519
pruemmer <ph_r@gmx.net>
2016-01-27 22:25:41 +0100

    benchmark collection

---
+++ LivenessProver/benchmarks/runAll.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+RUN=../run
+
+ulimit -t 1800
+
+for name in Israeli-Jalfon.txt \
+            bakery.txt \
+            firewire.txt \
+            herman-linear.txt \
+            Lehmann-Rabin.txt \
+            LR-philo.txt \
+            nim.txt \
+            rap.txt \
+            szymanski.txt; do
+
+    echo Benchmark: $name
+    
+    # Using incremental procedure and pre-computed invariants
+    /usr/bin/time -f "Runtime: %e" $RUN $name
+
+    # Using incremental procedure w/o pre-computed invariants
+    cp $name tempinput.txt
+    echo 'noPrecomputedInvariant;' >>tempinput.txt
+    /usr/bin/time -f "Runtime: %e" $RUN tempinput.txt
+
+    # Using monolithic procedure (w/o pre-computed invariants)
+    echo 'monolithicWitness;' >>tempinput.txt
+    /usr/bin/time -f "Runtime: %e" $RUN tempinput.txt
+
+done >benchmark.log 2>&1
