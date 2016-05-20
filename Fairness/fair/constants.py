868b738d1387f17fb02bbb0d1848a6c3d3fa8298
Ondra Lengal <ondra.lengal@gmail.com>
2016-05-20 15:04:10 +0200

    fair_encode: add a missing file

---
+++ Fairness/fair/constants.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+# special symbols
+SYMBOL_DELIM = 'delim'
+SYMBOL_ENABLED = 'enabled'
+SYMBOL_DISABLED = 'disabled'
+SYMBOL_CHOSEN = 'chosen'
+
+
+ENCODING_ALPHABET = {
+        SYMBOL_DELIM,
+        SYMBOL_ENABLED,
+        SYMBOL_DISABLED,
+        SYMBOL_CHOSEN,
+    }
+
+
+###############################################################################
+if __name__ == '__main__':
+    print("Library module")
