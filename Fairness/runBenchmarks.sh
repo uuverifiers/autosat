4f095fb6ffb986a84fa629a5ee0d36582d1aa9cd
Ondra Lengal <ondra.lengal@gmail.com>
2016-07-04 09:45:16 +0200

    benchmarks: benchmark running script

--- Fairness/runBenchmarks.sh
+++ Fairness/runBenchmarks.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 BENCHMARKS="\
+	benchmarks/currency_games/flip-game.txt \
 	benchmarks/moran/moran-linear-fair-AB.txt \
 	benchmarks/herman/herman-ring-selfloops.txt \
 	"
@@ -8,9 +9,27 @@ BENCHMARKS="\
 RUNCMD="./runFairFancy --silent"
 
 for i in ${BENCHMARKS} ; do
-	echo ${i}
 	TMPFILE=`mktemp /tmp/bench.XXXXXXXXXXX`
-	cat ${i} | grep -v '^logLevel' > ${TMPFILE}
+	OUTFILE=`mktemp /tmp/bench.XXXXXXXXXXX`
+	cat ${i} \
+		| grep -v '^logLevel' \
+		| grep -v '^explicitChecksUntilLength' \
+		> ${TMPFILE}
 
-	${RUNCMD} ${TMPFILE}
+	echo -n "Running ${i}:	"
+	STARTTIME=$(date +%s.%N)
+	${RUNCMD} ${TMPFILE} > ${OUTFILE}
+	ENDTIME=$(date +%s.%N)
+	DIFFTIME=$(echo "$ENDTIME - $STARTTIME" | bc)
+
+	grep -q "VERDICT: Player 2 can win from every initial configuration" ${OUTFILE}
+	ret=$?
+
+	if [ "$ret" -eq 0 ] ; then
+		echo -n "CORRECT:	"
+	else
+		echo -n "--FAILURE--:	"
+	fi
+
+	echo "${DIFFTIME}"
 done
