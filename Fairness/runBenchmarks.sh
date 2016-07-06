d9cdf4ffb0fa2e5d23629fba67457331a07a059c
Ondra Lengal <ondra.lengal@gmail.com>
2016-07-06 14:42:15 +0200

    benchmarks: add a script to run a single benchmark

--- Fairness/runBenchmarks.sh
+++ Fairness/runBenchmarks.sh
@@ -2,6 +2,7 @@
 
 BENCHMARKS="\
 	benchmarks/wolf-sheep/wolf-sheep-linear-nograss.txt \
+	benchmarks/clustering/clustering-line.txt \
 	benchmarks/currency_games/flip-game.txt \
 	benchmarks/herman/herman-linear-selfloops.txt \
 	benchmarks/herman/herman-ring-selfloops.txt \
@@ -14,33 +15,6 @@ BENCHMARKS="\
 	benchmarks/cell-cycle-switch/cell-cycle-switch-linear.txt \
 	"
 
-RUNCMD="./runFairFancy --silent"
-
 for i in ${BENCHMARKS} ; do
-	TMPFILE=`mktemp /tmp/bench.XXXXXXXXXXX`
-	OUTFILE=`mktemp /tmp/bench.XXXXXXXXXXX`
-	cat ${i} \
-		| grep -v '^logLevel' \
-		| grep -v '^explicitChecksUntilLength' \
-		> ${TMPFILE}
-	echo "logLevel: 1;" >> ${TMPFILE}
-
-	echo -n "$(date "+%y-%m-%d %H:%M:%S");	"
-	echo -n "${i};	"
-	STARTTIME=$(date +%s.%N)
-	${RUNCMD} ${TMPFILE} > ${OUTFILE}
-	ENDTIME=$(date +%s.%N)
-	DIFFTIME=$(echo "$ENDTIME - $STARTTIME" | bc)
-
-	grep -q "VERDICT: Player 2 can win from every" ${OUTFILE}
-	ret=$?
-
-	if [ "$ret" -eq 0 ] ; then
-		echo -e -n "\e[1;32mCORRECT\e[0m;	"
-	else
-		echo -e -n "--\e[1;31mFAILURE\e[0m--;	"
-	fi
-
-	echo -n "${DIFFTIME};	"
-	echo "( ${OUTFILE} )"
+	./runSingleBenchmark.sh ${i}
 done
